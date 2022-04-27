71281542dc956b63ffc78795e1bceb4bffdc4941
Larry Kagan <larry@superiocity.com>
2022-04-27 12:27:33 -0400

    Custom error handler middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
 const app = express();
 
 app.get('/', (req, res) => {
+    throw new Error('error');
     res.send('Welcome!');
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000
 
