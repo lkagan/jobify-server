59f0c13e91d73c5f43e601f724e2fa4d6c1644c6
Larry Kagan <larry@superiocity.com>
2022-05-07 20:24:21 -0400

    Fix bug in order of routes

--- server/routes/jobsRoutes.js
+++ server/routes/jobsRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.route('/').get(jobs.index);
 router.route('/').post(jobs.create);
+router.route('/stats').get(jobs.stats);
 router.route('/:id').patch(jobs.update);
 router.route('/:id').delete(jobs.destroy);
-router.route('/stats').get(jobs.stats);
 
 export default router;
