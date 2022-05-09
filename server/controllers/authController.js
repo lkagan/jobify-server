6449d7c42cc4af3af4f2648d6f7bd5fa14212a76
Larry Kagan <larry@superiocity.com>
2022-05-09 19:40:31 -0400

    Register user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,13 @@
-const register = (req, res) => {
-    res.send('register');
+import User from '../models/User.js';
+
+const register = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({user});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({msg: 'there was an error'});
+    }
 }
 
 const login = (req, res) => {
--- server/models/User.js
+++ server/models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide email'],
         validate: {
-            validator: validator.isEmpty,
+            validator: validator.isEmail,
             message: 'Please provide a valid email',
         },
         unique: true,
