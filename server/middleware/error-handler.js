9010942c6e8283e6bd9ea8abff27fcca9f5735fa
Larry Kagan <larry@superiocity.com>
2022-05-12 21:56:12 -0400

    Granular validation in auth controller

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,10 +1,10 @@
 import {StatusCodes} from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err);
+
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: 'Something went wrong, try again later'
+        msg: err.message || 'Something went wrong, try again later'
     };
 
     if (err.name === 'ValidationError') {
