83a4c12a67b2cc0253c058a280e61783bdca45b4
Larry Kagan <larry@superiocity.com>
2022-05-14 12:22:15 -0400

    Create JWT on user registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,7 +16,9 @@ const register = async (req, res) => {
     }
 
     user = await User.create({name, email, password});
-    res.status(StatusCodes.CREATED).json({user});
+    const token = user.createJWT();
+
+    res.status(StatusCodes.CREATED).json({user, token});
 }
 
 const login = (req, res) => {
