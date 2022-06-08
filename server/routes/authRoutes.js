e6df7f54993e5b8b9a7fe3a247f33fea6c1a9e81
Larry Kagan <larry@superiocity.com>
2022-06-07 21:29:33 -0400

    Setup authentication middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {register, login, update} from '../controllers/authController.js';
+import authenticateUser from '../middleware/auth.js'
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/update').patch(update);
+router.route('/update').patch(authenticateUser, update);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateMiddleware from './middleware/auth.js'
 import dotenv from 'dotenv';
 import connectDB from "./db/connect.js";
 import 'express-async-errors';
@@ -20,9 +21,11 @@ app.get('/', (req, res) => {
     res.send('Welcome!');
 });
 
+// Routers
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/jobs', jobsRoutes);
+app.use('/api/v1/jobs', authenticateMiddleware, jobsRoutes);
 
+// Middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
