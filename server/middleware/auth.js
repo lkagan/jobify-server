3f4bf867b70c9ded0acdb9ae3719c9a75debf519
Larry Kagan <larry@superiocity.com>
2022-06-09 11:35:17 -0400

    Authenticate JWT

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,19 @@
 import { UnauthenticatedError } from "../errors/index.js";
+import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
-    if (!req.headers.authorization) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader?.startsWith('Bearer')) {
         throw new UnauthenticatedError('Authentication Invalid');
     }
-    
-    next();
+
+    try {
+        req.user = jwt.verify(authHeader.split(' ')[1], process.env.JWT_SECRET);
+        next();
+    } catch (e) {
+        throw new UnauthenticatedError('Authentication invalid');
+    }
 }
 
 export default auth;
