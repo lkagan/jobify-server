20eac3fe2ab94f9ee7268992b8d97028381375c1
Larry Kagan <larry@superiocity.com>
2022-06-20 10:06:45 -0400

    Create jobs controller

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -1,9 +1,22 @@
+import { BadRequestError } from "../errors/index.js";
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+
 const index = (req, res) => {
     res.send('index');
 }
 
-const create = (req, res) => {
-    res.send('create');
+const create = async (req, res) => {
+    const { position, company } = req.body;
+
+    if (!position || !company) {
+        throw new BadRequestError('Please provide All Values');
+    }
+
+    req.body.createdBy = req.user.userId;
+
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({ job });
 }
 
 const destroy = (req, res) => {
