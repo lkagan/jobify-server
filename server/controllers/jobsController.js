ec7bbd3ffbf5b6b85340c7d69d16899a998ea210
Larry Kagan <larry@superiocity.com>
2022-07-01 20:17:58 -0400

    Include monthly stats in jobs stats

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -80,7 +80,24 @@ const stats = async (req, res) => {
         return acc;
     }, defaultStats);
 
-    let monthlyApplications = [];
+    let monthlyApplications = await Job.aggregate([
+        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+        {
+            $group: {
+                _id: {
+                    year: {
+                        $year: '$createdAt',
+                    },
+                    month: {
+                        $month: '$createdAt',
+                    },
+                },
+                count: { $sum: 1 },
+            }
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 },
+    ]);
 
     res.status(StatusCodes.OK).json({ stats, monthlyApplications });
 }
